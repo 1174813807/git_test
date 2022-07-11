8859a8c11e0f00890558e8a4106300d67aca1ffe
xiaoliu <liu@liu.com>
2022-07-12 00:29:33 +0800

    0.2 hot by xiaoliu

--- src/main/java/com/ly/git_test.java
+++ src/main/java/com/ly/git_test.java
@@ -4,7 +4,7 @@ public class git_test {
     public static void main(String[] args) {
         System.out.println("hello");
         System.out.println("hello");
-        System.out.println("");
+        System.out.println("123456");
         System.out.println("\"sout\" = " + "sout");
     }
 }
