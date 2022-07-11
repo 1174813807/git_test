426d0b82805d8cbcd57520de34f767e3efcf778b
xiaoliu <liu@liu.com>
2022-07-12 00:23:00 +0800

    0.1 hot by xiaoliu

--- src/main/java/com/ly/git_test.java
+++ src/main/java/com/ly/git_test.java
@@ -5,5 +5,6 @@ public class git_test {
         System.out.println("hello");
         System.out.println("hello");
         System.out.println("");
+        System.out.println("\"sout\" = " + "sout");
     }
 }
