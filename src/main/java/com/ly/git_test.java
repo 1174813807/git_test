2eab16638c67153e916880ce4806481294f0879f
xiaoliu <liu@liu.com>
2022-07-14 23:46:24 +0800

    0.4  push + test by xiaoliu

--- src/main/java/com/ly/git_test.java
+++ src/main/java/com/ly/git_test.java
@@ -6,5 +6,6 @@ public class git_test {
         System.out.println("hello");
         System.out.println("123456");
         System.out.println("\"sout\" = " + "sout");
+        System.out.println("2022年7月14日23:44:51  push test");
     }
 }
