8e05e73059a4f71f6ff1d64cb96381c2de1135b6
xiaoliu <liu@liu.com>
2022-07-15 17:57:40 +0800

    0.1 init  2022年7月15日17:57:10 by xiaoliu

--- src/main/java/com/ly/git_test.java
+++ src/main/java/com/ly/git_test.java
@@ -1,8 +1,12 @@
 package com.ly;
 
+/**
+ * 项目第一次开发，此次版本为初始版本
+ */
+
 public class git_test {
     public static void main(String[] args) {
-        sout
+        System.out.println("hello world ！");
 
 
     }
