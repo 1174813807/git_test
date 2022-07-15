0a2c52ebbcca1df2e9d42db52d9c9ba3cb0f1b1b
xiaoliu <liu@liu.com>
2022-07-15 17:54:00 +0800

    0.1 init by xiaoliu

--- src/main/java/com/ly/git_test.java
+++ src/main/java/com/ly/git_test.java
@@ -2,15 +2,7 @@ package com.ly;
 
 public class git_test {
     public static void main(String[] args) {
-        System.out.println("hello");
-        System.out.println("hello");
-        System.out.println("123456");
-        System.out.println("\"sout\" = " + "sout");
-        System.out.println("2022年7月14日23:44:51  push test");
-        System.out.println("2022年7月14日23:44:51   pull test");
-        System.out.println("2022年7月14日23:44:51   pull test");
-        System.out.println("2022年7月14日23:44:51   pull test");
 
-        sout。。。
+
     }
 }
